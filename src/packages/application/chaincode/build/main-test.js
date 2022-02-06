96302640730320b74b91571417a93c463f6440e8
Renat Gubaev <renat.gubaev@gmail.com>
2022-02-06 21:30:31 +0300

    no message

--- src/packages/application/chaincode/build/main-test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const AppSettings_1 = require("./src/AppSettings");
-let settings = new AppSettings_1.AppSettings();
-async function bootstrap() {
-}
-bootstrap();
