2a9c30ed827332f4292aea5a49110e8627262908
Renat Gubaev <renat.gubaev@gmail.com>
2022-02-15 15:12:10 +0300

    no message

--- ledger/IGenesis.ts
+++ ledger/IGenesis.ts
@@ -3,3 +3,7 @@ export interface IGenesis {
     rootCompanyUid: string;
     createdDate: Date;
 }
+
+import { TransportCryptoManagerEd25519 } from "@ts-core/common/transport/crypto";
+export const ROOT_USER_CRYPTO_KEY = 'e365007e85508c6b44d5101a1d59d0061a48fd1bcd393186ccb5e7ae938a59a8';
+export const ROOT_USER_CRYPTO_ALGORITHM = TransportCryptoManagerEd25519.ALGORITHM;
